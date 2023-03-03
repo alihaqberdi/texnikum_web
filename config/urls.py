fc74476481895781a563f68255849f9606211377
AlisheyxPy <alhppypro@gmail.com>
2023-03-03 08:59:38 +0500

    initial commit2

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .settings import DEBUG
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+if DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
